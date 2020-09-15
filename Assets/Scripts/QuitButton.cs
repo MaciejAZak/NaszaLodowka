c9b282926f1285062406870a0c08702c32a9e2ce
Zh00c3q <maciej.a.zak@gmail.com>
2020-09-15 15:11:22 +0200

    add options screen with resolution changer

--- Assets/Scripts/QuitButton.cs
+++ Assets/Scripts/QuitButton.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuitButton : MonoBehaviour
 {
+
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OptionsScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
---
+++ Assets/Scripts/ScreenResolution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Screen resolution is: " + Screen.currentResolution);
+        Debug.Log("Current width is: " + Screen.width);
+        Debug.Log("Current height is: " + Screen.height);
+        Debug.Log("Currently available resolutions are: " + Screen.resolutions);
+        Debug.Log("Currently fullscreen? " + Screen.fullScreen);
+    }
+}
