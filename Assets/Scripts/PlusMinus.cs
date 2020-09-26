dc89626d0b3bb137ae512934677a079425b14507
Zh00c3q <maciej.a.zak@gmail.com>
2020-09-26 14:10:12 +0200

    add a prefab of ingredient (GameObject) with Plus and Minus buttons and scripts

---
+++ Assets/Scripts/PlusMinus.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlusMinus : MonoBehaviour
+{
+
+    bool Active = false;
+    AmountScript currentAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmount = FindObjectOfType<AmountScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Clicked();
+    }
+
+    void Clicked()
+    {
+        if (Input.GetMouseButtonDown(0) && Active && this.name == "PlusButton")
+        {
+
+            Debug.Log(this.name + "clicked Plus");
+            currentAmount.GetComponent<TextMeshPro>().text = "1";
+        }
+        else if (Input.GetMouseButtonDown(0) && Active && this.name == "MinusButton")
+        {
+            Debug.Log(this.name + "clicked Minus");
+            currentAmount.GetComponent<TextMeshPro>().text = "-1";
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        Active = true;
+    }
+
+    private void OnMouseExit()
+    {
+        Active = false;
+    }
+
+}
